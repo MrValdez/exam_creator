3b583e19b06d83c9ae45bfd11b7043a2ab9f4d21
MrValdez <shunyvaldez@gmail.com>
2017-03-09 10:55:17 +0800

    refactored code. added documentation.

--- exam_creator.py
+++ exam_creator.py
@@ -2,6 +2,18 @@ import random
 import string
 
 def extract_questions(file):
+    '''
+    1. Open file
+    2. Separate each question into list (one question per element)
+    3. Expand each question into its variations
+    4. Return the questions in the format: 
+        [
+            [question1_variation1, question1_variation2, ..., question1_variationN],
+            [question2_variation1, question2_variation2, ..., question2_variationN],
+            [question3_variation1, question3_variation2, ..., question3_variationN],
+            ...
+        ]
+    '''
     with open(file) as f:
         file = f.read()
 
@@ -30,46 +42,66 @@ def extract_questions(file):
 
     return questions
 
-def generate_test_paper(exam1, exam2):
+def generate_test_paper(exam1, exam2, student_size = 60, part2_size = 2):
+    '''
+    1. Load the header file that is common on all test papers
+    2. For each student,
+        2.a. Choose a random question variation for Part 1
+        2.b. Choose 2 random question variations for Part 2
+        2.c. Determine the filename identifier for each question variations
+        2.d. Add the questions to the test paper
+    3. Return all the test papers
+    '''
     with open("header.txt") as f:
         header = f.read()
 
-    test_papers = []
+    Part1_seperator = "\n---\nFilename: Part1.py\n"
+    Part2_seperator = "\n---\nFilename: Part2_{}.py\n"
 
-    student_size = 60
-    part2_size = 2
+    test_papers = []
     for i in range(student_size):
-        test_paper = str(header)
-        test_paper += "\n---\nFilename: Part1.py\n"
         random_questions = random.choice(exam2)
-        test_paper += random.choice(random_questions)
+        question = random.choice(random_questions)
+
+        test_paper = str(header)
+        test_paper += Part1_seperator
+        test_paper += question
 
         random_questions = random.sample(exam1, part2_size)
         for question_no, question_variations in enumerate(random_questions):
             question = random.choice(question_variations)
 
-            filename = "Part2_{}.py"
-            filename = filename.format(string.ascii_uppercase[question_no])
-            test_paper += "\n---\nFilename: {}\n".format(filename)
+            filename_identifier = string.ascii_uppercase[question_no]
+            test_paper += Part2_seperator.format(filename_identifier)
             test_paper += question
 
         test_papers.append(test_paper)
     return test_papers
 
-exam1 = extract_questions("exam.txt")
-exam2 = extract_questions("exam2.txt")
+def save_questions(questions, seperator = "\n=========\n"):
+    """
+    save all the question variations for future reference
+    """
+    with open("questions.txt", "w") as f:
+        question_variations = [seperator.join(question) for question in questions] 
+        output = seperator.join(question_variations)
+        f.write(output)
 
-questions = exam1 + exam2
+    question_size = len(questions)
+    question_variations = sum([len(question) for question in questions])
 
-test_papers = generate_test_paper(exam1, exam2)
+    print("{} questions saved.\n{} total variations created.".format(question_size, question_variations))
 
-# save the questions for future reference
-with open("questions.txt", "w") as f:
-    question_variations = ["\n=========\n".join(question) for question in questions] 
-    output = "\n=========\n".join(question_variations)
-    f.write(output)
+def save_test_papers(test_papers):
+    with open("test papers.txt", "w") as f:
+        f.write("\n\n\n\n\n".join(test_papers))
 
-print("{} questions saved".format(len(questions)))
+if __name__ == "__main__":
+    exam1 = extract_questions("exam.txt")
+    exam2 = extract_questions("exam2.txt")
 
-with open("test papers.txt", "w") as f:
-    f.write("\n\n\n\n\n".join(test_papers))
+    questions = exam1 + exam2
+    save_questions(questions)
+
+    test_papers = generate_test_paper(exam1, exam2)
+    save_test_papers(test_papers)
